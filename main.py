7e254a35d2e674fcf026f3b552b958aaa7782ede
AstaMotrenko <118980102+AstaMotrenko@users.noreply.github.com>
2023-01-16 22:22:31 +0200

    Class Paddle and drawing it

--- main.py
+++ main.py
@@ -5,10 +5,33 @@ TITLE = "Arkanoid"
 WIDTH = 800
 HEIGHT = 500
 
+class Paddle:
+    def __init__(self, x, y, sprite):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.paddle = Actor(sprite)
+        self.paddle.x = x
+        self.paddle.y = y
+
+    def draw(self): self.paddle.draw()
+
+    def update(self):
+        if game_status == 1:
+            self.paddle.x = pg.mouse.get_pos()[0]
+        else:
+            self.paddle.x = 1000
+
 def draw():
-    pass
+    global game_status
+    screen.blit("background.jpeg", (0,0))
+    paddle.draw()
 
 def update():
-    pass
+    paddle.update()
+
+
+game_status = 1
+paddle = Paddle(250, 450, 'paddle.png')
 
 pgzrun.go()
