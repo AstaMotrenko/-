a7824e7cc21313b181d9b1aee179a9ea866a79a5
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-01-18 03:19:48 +0200

    Improving graphics, adding stability to platforms

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import pgzrun
 import pygame as pg
 
+# Window options
 TITLE = "Arkanoid"
 WIDTH = 800
 HEIGHT = 500
 
+# Add Paddle class
 class Paddle:
     def __init__(self, x, y, sprite):
-        self.x = x
-        self.y = y
         self.sprite = sprite
-        self.paddle = Actor(sprite)
+        self.paddle = Actor(sprite,anchor=(50,0))
         self.paddle.x = x
         self.paddle.y = y
 
-    def draw(self): self.paddle.draw()
+
+    def draw(self):
+        self.paddle.draw()
+
 
     def update(self):
+        # Check game status and hold position by cursor
         if game_status == 1:
             self.paddle.x = pg.mouse.get_pos()[0]
-        else:
-            self.paddle.x = 1000
+        elif game_status != 1:
+            self.paddle.x = 0
+        # Bouncing the ball from the collider
+        if self.paddle.colliderect(ball.ball):
+            ball.speed_y *= -1
 
 
 class Ball:
     def __init__(self, x, y, sprite, speed_x, speed_y):
-        self.x = x
-        self.y = y
         self.sprite = sprite
         self.ball = Actor(sprite)
         self.ball.x = x
@@ -34,87 +39,98 @@ class Ball:
         self.speed_x = speed_x
         self.speed_y = speed_y
 
-    def draw(self): self.ball.draw()
+
+    def draw(self):
+        self.ball.draw()
+
 
     def update(self):
         if game_status == 1:
+            # Ball movement
             self.ball.x += self.speed_x * 2
             self.ball.y += self.speed_y * 2
-
+            # Bouncing off the borders
             if self.ball.x <= 0 or self.ball.x >= WIDTH:
                 self.speed_x *= -1
             if self.ball.y <= 0:
                 self.speed_y *= -1
-        else: self.ball.x = 1000
+        else:
+            self.ball.x = 0
+
 
 class Obstacle:
     def __init__(self, x, y, sprites ,hits_to_destroy):
-        self.x = x
-        self.y = y
         self.obstacle = Actor(sprites)
         self.obstacle.x = x
         self.obstacle.y = y
-        self.obstacle.hitstodestroy = hits_to_destroy
+        self.obstacle.hits_to_destroy = hits_to_destroy
+
 
     def draw(self):
         self.obstacle.draw()
 
-
+# Function to create obstacles
 def draw_obs(obstacles):
-    for i in range(6):
-        x = 100 + i * 120
+    for i in range(7):
+        x = 70 + i * 110
         y = 75
         obstacle = Obstacle(x,y,sprites[2],3)
-        obstacles.append(obstacle.obstacle)
+        obstacles.append(obstacle)
 
-    for i in range(6):
-        x = 100 + i * 120
-        y = 125
+    for i in range(7):
+        x = 70 + i * 110
+        y = 115
         obstacle = Obstacle(x,y,sprites[2],3)
-        obstacles.append(obstacle.obstacle)
+        obstacles.append(obstacle)
 
-    for i in range(6):
-        x = 100 + i * 120
-        y = 175
+    for i in range(7):
+        x = 70 + i * 110
+        y = 155
         obstacle = Obstacle(x,y,sprites[1],2)
-        obstacles.append(obstacle.obstacle)
+        obstacles.append(obstacle)
 
-    for i in range(6):
-        x = 100 + i * 120
+    for i in range(7):
+        x = 70 + i * 110
         y = 225
         obstacle = Obstacle(x,y,sprites[0],1)
-        obstacles.append(obstacle.obstacle)
+        obstacles.append(obstacle)
+
+    for i in range(4):
+        x = 70 + i * 220
+        y = 265
+        obstacle = Obstacle(x,y,sprites[1],2)
+        obstacles.append(obstacle)
 
 
 def draw():
     global game_status
-    screen.blit("background.jpeg", (0,0))
+    screen.blit("background.png", (0,0))
     paddle.draw()
     ball.draw()
+
     for obstacle in obstacles:
         obstacle.draw()
     for heart in hearts:
         heart.draw()
-    if game_status == 0: screen.blit('gameover.png', (0,0))
-    if game_status == 2: screen.blit('win.png', (0,0))
+
+    if game_status == 0:
+        screen.blit('gameover.png', (0,0))
+
+    if game_status == 2:
+        screen.blit('win.png', (0,0))
 
 
 def update():
     global lives, game_status
     paddle.update()
     ball.update()
-
-    if paddle.paddle.colliderect(ball.ball):
-        ball.speed_y *= -1
-
+    # Check hits to destroy obstacle and bouncing the ball
     for obstacle in obstacles:
-        if ball.ball.colliderect(obstacle):
+        if ball.ball.colliderect(obstacle.obstacle):
             ball.speed_y *= -1
-            obstacles.remove(obstacle)
-#            obstacle.obstacle.hits_to_destroy -= 1
-#            if obstacle.obstacle.hits_to_destroy == 0: obstacles.remove(obstacle)
-
-
+            obstacle.obstacle.hits_to_destroy -= 1
+            if obstacle.obstacle.hits_to_destroy == 0: obstacles.remove(obstacle)
+    # Loss of life in case of a miss
     if ball.ball.y >= HEIGHT:
         lives -= 1
         ball.ball.x = 400
@@ -129,15 +145,16 @@ def update():
     if not obstacles and game_status != 0:
         game_status = 2
 
-
+# Creating lives
 lives = 3
 hearts = []
 for i in range(lives):
     heart = Actor('heart.png')
-    heart.x = 30 + i * 50
-    heart.y = 25
+    heart.x = 30 + i * 55
+    heart.y = 35
     hearts.append(heart)
 
+# Variables and initialization
 game_status = 1
 paddle = Paddle(250, 450, 'paddle.png')
 ball = Ball(300, 400, 'ball.png', 2, -2)
